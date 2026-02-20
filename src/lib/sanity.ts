82f58d18ba420330b20f794aa3f8886d756c33f3
alexpetre27 <147337105+alexpetre27@users.noreply.github.com>
2026-02-21 00:05:23 +0200

    updated projects page

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,22 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "more0rqr",
+  projectId: "zcrc78yf",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: true,
 });
+export async function getProjects() {
+  return await client.fetch(`
+    *[_type == "project"] | order(_createdAt desc) {
+      title,
+      slug,
+      category,
+      description,
+      "image": image.asset->url,
+      link,
+      github,
+      tags
+    }
+  `);
+}
